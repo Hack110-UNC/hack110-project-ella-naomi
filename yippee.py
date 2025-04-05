17e01af5bdf6b7dad45b8534fcf017508840e90b
Naomi Lytle <nadlytle@unc.edu>
2025-04-05 12:36:50 -0400

    new changes

--- yippee.py
+++ yippee.py
@@ -1,6 +1,17 @@
 """This is Ella and Naomi's project"""
 
 import pygame
+import random
 import sys
 
 pygame.init()
+
+# dimensions
+WIDTH, HEIGHT = 400, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("FantasyGame! - Simplified")
+
+# 
+font = pygame.font.Font(None, 48)
+bg_color = (30, 30, 30)
+text_color = (255, 255, 255)

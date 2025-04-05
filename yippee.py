6a63720e550cd0c2602234134087766b85cf64a9
Naomi Lytle <nadlytle@unc.edu>
2025-04-05 12:41:21 -0400

    test change

--- yippee.py
+++ yippee.py
@@ -6,3 +6,7 @@ import sys
 
 pygame.init()
 
+# Screen dimensions and settings
+WIDTH, HEIGHT = 400, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Mermaid Game!")
